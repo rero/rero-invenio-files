55ed0cd7ce93fb5dc59c83b8c4d06a43e6ff19e0
Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>
2024-05-15 12:09:01 +0200

    invenio: new version
    * Updates dependencies.
    * Replaces `safety` with `pip-audit` for python vulnerabilities checks.
    * Uses python 3.12.
    
    Co-Authored-by: Peter Weber <peter.weber@rero.ch>

--- run-tests.sh
+++ run-tests.sh
@@ -32,20 +32,32 @@ function cleanup() {
 }
 trap cleanup EXIT
 
+pip_audit_exceptions=""
+add_exceptions() {
+  pip_audit_exceptions="$pip_audit_exceptions --ignore-vuln $1"""
+}
+
 # python -m check_manifest
 python -m sphinx.cmd.build -qnNW docs docs/_build/html
 
-safety_exceptions="-i 51668 -i 42194 -i 62019 -i 67599 -i 51457"
-msg=$(safety check -o text ${safety_exceptions}) || {
-    echo "Safety vulnerabilites found for packages:" $(safety check -o bare ${safety_exceptions})
-    echo "Run: \"safety check -o screen ${safety_exceptions} | grep -i vulnerability\" for more details"
-    exit 1
-  }
+# cryptography 43.0.3  GHSA-79v4-65xg-pq4g 44.0.1
+add_exceptions "GHSA-79v4-65xg-pq4g"
+# py           1.11.0  PYSEC-2022-42969
+add_exceptions "PYSEC-2022-42969"
+# werkzeug     2.2.3   PYSEC-2023-221      2.3.8,3.0.1
+add_exceptions "PYSEC-2023-221"
+# werkzeug     2.2.3   GHSA-2g68-c3qc-8985 3.0.3
+add_exceptions "GHSA-2g68-c3qc-8985"
+# werkzeug     2.2.3   GHSA-f9vj-2wh5-fj8j 3.0.6
+add_exceptions "GHSA-f9vj-2wh5-fj8j"
+# werkzeug     2.2.3   GHSA-q34m-jh98-gwm2 3.0.6
+add_exceptions "GHSA-q34m-jh98-gwm2"
+PIPAPI_PYTHON_LOCATION=`which python` pip-audit ${pip_audit_exceptions}
 
 autoflake -r --remove-all-unused-imports --ignore-init-module-imports --quiet .
 
 # TODO: Remove services below that are not neeed (fix also the usage note).
-eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-elasticsearch} --cache ${CACHE:-redis} --mq ${MQ:-rabbitmq} --env)"
+docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-elasticsearch} --cache ${CACHE:-redis} --mq ${MQ:-rabbitmq} --env
 python -m pytest
 tests_exit_code=$?
 python -m sphinx.cmd.build -qnNW -b doctest docs docs/_build/doctest
